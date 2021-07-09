e4e7be47e4a080b1ad334b9015cdfee6d3a82fb1
Pramodini Karwande <k.pramodini@gmail.com>
2021-07-09 00:45:31 -0400

    Updated Wrapper function and updated model variables

--- wrapper.R
+++ wrapper.R
@@ -2,3 +2,13 @@ rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Monday.html",
                   params = list(dayOfTheWeek = 1))
 rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Tuesday.html", 
                   params = list(dayOfTheWeek = 2))
+rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Wednesday.html", 
+                  params = list(dayOfTheWeek = 3))
+rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Thursday.html", 
+                  params = list(dayOfTheWeek = 4))
+rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Friday.html", 
+                  params = list(dayOfTheWeek = 5))
+rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Saturday.html", 
+                  params = list(dayOfTheWeek = 6))
+rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Sunday.html", 
+                  params = list(dayOfTheWeek = 7))
