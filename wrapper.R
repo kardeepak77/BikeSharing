f80b060d914a213f6c824d5cbba13399eda36157
Pramodini Karwande <k.pramodini@gmail.com>
2021-07-10 11:22:31 -0400

    committing wrapper forloop for each day

--- wrapper.R
+++ wrapper.R
@@ -1,14 +1,7 @@
-rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Monday.html", 
-                  params = list(dayOfTheWeek = 1))
-rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Tuesday.html", 
-                  params = list(dayOfTheWeek = 2))
-rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Wednesday.html", 
-                  params = list(dayOfTheWeek = 3))
-rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Thursday.html", 
-                  params = list(dayOfTheWeek = 4))
-rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Friday.html", 
-                  params = list(dayOfTheWeek = 5))
-rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Saturday.html", 
-                  params = list(dayOfTheWeek = 6))
-rmarkdown::render("weeklyAnalysis.Rmd", output_file = "Sunday.html", 
-                  params = list(dayOfTheWeek = 7))
+
+weekday <- c("Monday.html", "Tuesday.html", "Wednesday.html", "Thursday.html", "Friday.html", "Saturday.html", "Sunday.html")
+
+for(i in 1:length(weekday)) {
+  rmarkdown::render("weeklyAnalysis.Rmd", output_file = weekday[i], 
+                  params = list(dayOfTheWeek = i))
+}
