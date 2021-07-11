f6f9af19341eb08e4c6b5d49749deaf98ab47085
Pramodini Karwande <k.pramodini@gmail.com>
2021-07-11 01:02:39 -0400

    updated file extentions

--- wrapper.R
+++ wrapper.R
@@ -1,5 +1,5 @@
 
-weekday <- c("Sunday.html", "Monday.html", "Tuesday.html", "Wednesday.html", "Thursday.html", "Friday.html", "Saturday.html")
+weekday <- c("Sunday.md", "Monday.md", "Tuesday.md", "Wednesday.md", "Thursday.md", "Friday.md", "Saturday.md")
 
 for(i in 0:(length(weekday)-1)) {
   rmarkdown::render("weeklyAnalysis.Rmd", output_file = weekday[i+1], params = list(dayOfTheWeek = i))
